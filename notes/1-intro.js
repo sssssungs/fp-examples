62b185cfa54fb11db57f833d4a74eddf1a257f62
sssssungs <32057999+sssssungs@users.noreply.github.com>
2021-02-07 11:23:08 +0900

    prettier

--- notes/1-intro.js
+++ notes/1-intro.js
@@ -1,6 +1,6 @@
 {
   function addMaker(a) {
-    return function(b) {
+    return function (b) {
       return a + b;
     };
   }
@@ -15,7 +15,7 @@
     { id: 4, name: "BC", age: 20 },
     { id: 5, name: "PQ", age: 35 },
     { id: 6, name: "SS", age: 24 },
-    { id: 7, name: "AH", age: 28 }
+    { id: 7, name: "AH", age: 28 },
   ];
 
   let temp_user = [];
@@ -51,7 +51,7 @@
     return new_list;
   }
 
-  let user_under_30 = filter(users, user => user.age < 30);
+  let user_under_30 = filter(users, (user) => user.age < 30);
   // console.log("user_under_30", user_under_30);
 
   // map을 생성함.
@@ -63,20 +63,20 @@
     return new_list;
   }
   let ages_under_30 = map(
-    filter(users, user => user.age < 30),
-    user => user.age
+    filter(users, (user) => user.age < 30),
+    (user) => user.age
   );
 
   let names_under_30 = map(
-    filter(users, user => user.age < 30),
-    user => user.name
+    filter(users, (user) => user.age < 30),
+    (user) => user.name
   );
   // console.log("ages_under_30", ages_under_30);
   // console.log("names_under_30", names_under_30);
 
   // 함수를 생성하는 함수를 만든다.
   function bvalue(key) {
-    return function(obj) {
+    return function (obj) {
       return obj[key];
     };
   }
@@ -114,63 +114,77 @@
   console.log(findBy("name", users, "PQ"));
 
   function User(id, name, age) {
-    this.getId = function(){ return id;}
-    this.getName = function(){ return name;}
-    this.getAge = function(){ return age;}
+    this.getId = function () {
+      return id;
+    };
+    this.getName = function () {
+      return name;
+    };
+    this.getAge = function () {
+      return age;
+    };
   }
 
   const users2 = [
-      new User(1, 'QQ', 33),
-      new User(2, 'AA', 23),
-      new User(3, 'WW', 23),
-      new User(4, 'RR', 31),
-      new User(5, 'TT', 36),
-      new User(6, 'ZZ', 32),
-      new User(7, 'QQ', 33),
-  ]
+    new User(1, "QQ", 33),
+    new User(2, "AA", 23),
+    new User(3, "WW", 23),
+    new User(4, "RR", 31),
+    new User(5, "TT", 36),
+    new User(6, "ZZ", 32),
+    new User(7, "QQ", 33),
+  ];
 
   function find(list, predicate) {
-    for(let i = 0, len = list.length; i< len; i++) {
-      if(predicate(list[i])) return list[i];
+    for (let i = 0, len = list.length; i < len; i++) {
+      if (predicate(list[i])) return list[i];
     }
   }
 
-  console.log('users2', find(users2, user => user.getAge() === 33).getName());
-  console.log('user', find(users, user => user.name.indexOf('I')));
+  console.log("users2", find(users2, (user) => user.getAge() === 33).getName());
+  console.log(
+    "user",
+    find(users, (user) => user.name.indexOf("I"))
+  );
 
   // 1-25 b match1으로 predicate 만들기 (closure)
-  const bmatch1 = (key, val) => function (obj) {
-    return obj[key] === val;
-  };
+  const bmatch1 = (key, val) =>
+    function (obj) {
+      return obj[key] === val;
+    };
 
-  console.log('find with bmatch', find(users, bmatch1('id', 5)));
-  console.log('filter with bmatch', filter(users, bmatch1('age', 28)))
-  console.log('map with bmatch', map(users, bmatch1('age', 28)))
-
+  console.log("find with bmatch", find(users, bmatch1("id", 5)));
+  console.log("filter with bmatch", filter(users, bmatch1("age", 28)));
+  console.log("map with bmatch", map(users, bmatch1("age", 28)));
 
   // 1-27 bmatch
-  const object = (key,val) => {
+  const object = (key, val) => {
     const obj = {};
     obj[key] = val;
     return obj;
-  }
+  };
 
   const match = (obj, obj2) => {
-    for(let key in obj2) {
-      if(obj[key] !== obj2[key]) return false;
+    for (let key in obj2) {
+      if (obj[key] !== obj2[key]) return false;
     }
     return true;
-  }
+  };
 
   function bmatch(obj2, val) {
-    console.log('arguments.length', arguments)
-    if(arguments.length === 2) obj2 = object(obj2, val);
-    console.log('obj2', obj2)
-    return function(obj) {
+    console.log("arguments.length", arguments);
+    if (arguments.length === 2) obj2 = object(obj2, val);
+    console.log("obj2", obj2);
+    return function (obj) {
       return match(obj, obj2);
-    }
+    };
   }
 
-  console.log('match', match(find(users, bmatch('id',3), find(users, bmatch('name', 'IU')))))
+  console.log(
+    "match",
+    match(find(users, bmatch("id", 3), find(users, bmatch("name", "IU"))))
+  );
 
+  // 최대한 true/false로 리턴하게 만들어서 if 조건을 태울수 있도록 한다.
+  console.log("find same object", find(users, bmatch({ id: 3, name: "IU" })));
 }
