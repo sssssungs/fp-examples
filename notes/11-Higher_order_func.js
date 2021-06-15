631e9e306ab168ba292c145bb1e9ac8ec45327dc
sssssungs <32057999+sssssungs@users.noreply.github.com>
2021-06-15 21:42:35 +0900

    High order function in Partial js

--- notes/11-Higher_order_func.js
+++ notes/11-Higher_order_func.js
@@ -30,7 +30,7 @@ function partialjs() {
   });
 } // map에 인자를 여러개 넘기면 iteratee 에게도 인자로 넘겨준다.
 // partialjs의 주요 고차 함수들은, 추가로 사용하고자 하는 인자를 함께 넘길수 있도록 되어 있다.
-// 메인 인자인 [1,2,3] 이전에 인자들을 넘기면 보조함수의 기본 인자들인 val, i, list등의 이전 인자로 전달해준다
+// 메인 인자인 [1,2,3] 이전에 인자들을 넘기면 보조함수의 기본 인자들인 val, i, list 등의 이전 인자로 전달해준다
 console.log("underscorejs1", underscorejs1());
 console.log("underscorejs2", underscorejs2());
 console.log("partialjs", partialjs());
@@ -45,8 +45,8 @@ function mult(a, b) {
   return a * b;
 }
 
-var mult_all = _.map(function () {
-  // map의 iteratee argument: value, index, list
+var mult_all = _.map(function (a, b, val) {
+  // map의 iteratee argument: value, index, list (뒤에서부터 3개는 무조건 여기로 들어감)
   return _.reduce(_.initial(arguments, 2), mult); // initial : 뒤에서부터 n개 제거하고 나머지 반환한다.
 });
 console.log("mult_all", mult_all(10, 10, 2, [1, 2, 3]));
