ab6d0b1a4d270de9e22a4211d227ee7cb1ad4af7
sssssungs <32057999+sssssungs@users.noreply.github.com>
2021-06-19 16:03:14 +0900

    _.indent

--- notes/12-Pipeline2.js
+++ notes/12-Pipeline2.js
@@ -16,3 +16,53 @@ _.go.call(
 );
 
 console.log(user);
+
+console.log("=============================");
+//코드 5-33 indent 란 ?
+var f1 = _.indent(
+  function () {
+    console.log("1 ===>", this, arguments, this.parent);
+    return "hi1";
+  },
+  function () {
+    console.log("2 ===>", this, arguments, this.parent);
+  }
+);
+
+f1.call({ a: 10 }, 1, 2);
+
+var f2 = _.indent(
+  function (a) {
+    this.b = a + 10;
+  },
+  function () {},
+  function () {},
+  function () {
+    console.log(this.b);
+  }
+);
+
+f2(5);
+f2(7);
+console.log("--------------");
+// 코드 5-36 indent 를 두번이상 하기.
+var f3 = _.indent(
+  function (a) {
+    this.b = a + 10;
+  },
+  _.indent(
+    function () {
+      this.b = 20;
+      console.log(this.b);
+      console.log(this.parent.b);
+    },
+    function () {
+      console.log(this.parent.arguments);
+    }
+  ),
+  function () {
+    console.log(this.b);
+  }
+);
+
+f3(5);
