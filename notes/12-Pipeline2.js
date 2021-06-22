d026ebe33ebb6d1008407bfdf288836021bb90ac
sssssungs <32057999+sssssungs@users.noreply.github.com>
2021-06-22 21:15:29 +0900

    Async note

--- notes/12-Pipeline2.js
+++ notes/12-Pipeline2.js
@@ -53,16 +53,46 @@ var f3 = _.indent(
   _.indent(
     function () {
       this.b = 20;
-      console.log(this.b);
-      console.log(this.parent.b);
+      console.log("this.b", this.b);
+      console.log("this.parent.b", this.parent.b);
     },
     function () {
-      console.log(this.parent.arguments);
+      console.log("this.parent.arguments", this.parent.arguments);
     }
   ),
   function () {
-    console.log(this.b);
+    console.log("this.b", this.b);
   }
 );
 
 f3(5);
+console.log("===========================================");
+
+// 5.4.4 무조건 비동기로 시작하는 async
+// 코드 5-37
+// _.go
+//   .async(1, function (a) {
+//     return a;
+//   })
+//   .then(console.log);
+// console.log(11); // 무조건 비동기로 동작하도록 해서 11이 먼저 보여지고 위에 1이 보여짐.
+
+// 코드 5-38 비동기제어
+_.go(
+  20,
+  _.branch(
+    // branch 내부에서는 비동기가 제어가 된다.
+    function (a) {
+      return new Promise(function (resolve) {
+        resolve(a + 10);
+      });
+    },
+    function (a) {
+      // a+10 을 기다림
+      console.log("두번째", a);
+      return a * a;
+    },
+    console.log
+  ),
+  console.log // 20은 즉시 내려와서 찍힘.
+); // >>>> 결과 20 , 30 , 900 -- 30보다 20이 먼저 찍혔다.
